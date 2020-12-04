938e432f3c3fd8dd5720998edbd04c847092ea48
darkwizard242 <am900820@gmail.com>
2020-12-04 13:29:29 -0500

    testinfra functions refactored as part of migration

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,31 +6,25 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_ruby_package_installed(host):
-    assert host.package("ruby-full").is_installed or \
-      host.package("ruby").is_installed
-
-
-def test_ruby_binary_exists(host):
-    host.file('/usr/bin/ruby').exists
-
-
-def test_ruby_binary_file(host):
-    host.file('/usr/bin/ruby').is_file
-
-
-def test_ruby_binary_which(host):
-    assert host.check_output('which ruby') == '/usr/bin/ruby'
+PACKAGE_BINARY = '/usr/local/bin/serverspec-init'
 
 
 def test_serverspec_binary_exists(host):
-    host.file('/usr/local/bin/serverspec-init').exists
+    """
+    Tests if serverspec-init binary exists.
+    """
+    assert host.file(PACKAGE_BINARY).exists
 
 
 def test_serverspec_binary_file(host):
-    host.file('/usr/local/bin/serverspec-init').is_file
+    """
+    Tests if serverspec-init binary is file type.
+    """
+    assert host.file(PACKAGE_BINARY).is_file
 
 
 def test_serverspec_binary_which(host):
-    assert host.check_output('which serverspec-init') == \
-     '/usr/local/bin/serverspec-init'
+    """
+    Tests the output to confirm serverspec-init's binary location.
+    """
+    assert host.check_output('which serverspec-init') == PACKAGE_BINARY
